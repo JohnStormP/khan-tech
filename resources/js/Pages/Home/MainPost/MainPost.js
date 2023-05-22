1839957ac8eaf1442e84adf9dcdcfdfd368f307f
Http <httpskylogic@gmail.com>
2023-05-23 01:37:06 +0300

    feat: frontend main post

---
+++ resources/js/Pages/Home/MainPost/MainPost.js
@@ -0,0 +1,12 @@
+export default {
+    props: {
+        main: Object
+    },
+    data() {
+        return {
+            postDate: '28 Feb 2021',
+            author: 'Jim Sullivan',
+            readTime: '6 min read'
+        }
+    }
+}
---
+++ resources/js/Pages/Home/MainPost/MainPost.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="main" class="mid-content">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="main-post" class="post">
+                    <div id="main-post-img">
+                        <img v-if="main" :src="main.image"
+                             alt="yoga studio">
+                    </div>
+                    <div id="main-post-text">
+                        <div class="post-category">
+                            <h4 class="bold"><a v-if="main" href="#">{{ main.category_id }}</a></h4>
+                        </div>
+                        <div class="post-title">
+                            <h2 class="medium" v-if="main">{{ main.title }}</h2>
+                        </div>
+                        <div class="post-text" v-if="main">
+                            {{ main.body }}
+                        </div>
+                        <div class="post-footer">
+                            <div class="post-date">
+                                {{ postDate }}
+                            </div>
+                            <div class="post-author">
+                                <a href="">{{ author }}</a>
+                            </div>
+                            <div class="post-time-read">
+                                <img src="/storage/img/Clock.svg" class="clock" alt="clock">{{ readTime }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script src="./MainPost.js"></script>
