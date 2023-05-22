233aaa0e2bf9775cf65b82290489df61601524fc
Http <httpskylogic@gmail.com>
2023-05-23 01:36:01 +0300

    feat: frontend Top random post

---
+++ resources/js/Pages/Home/RandomPost/RandomPost.js
@@ -0,0 +1,13 @@
+export default {
+    props: {
+        randomPost: Object
+
+    },
+    data() {
+        return {
+            postDate: '28 Feb 2021',
+            author: 'Jim Sullivan',
+            readTime: '6 min read'
+        }
+    }
+}
---
+++ resources/js/Pages/Home/RandomPost/RandomPost.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-md-8">
+        <div id="random-post" class="post">
+            <div id="random-post-img">
+                <img v-if="randomPost" :src="randomPost.image"
+                     alt="image">
+            </div>
+
+            <div id="random-post-text">
+                <div class="post-category">
+                    <h4 class="bold"><a v-if="randomPost" href="#">{{ randomPost.category_id }}</a></h4>
+                </div>
+                <div class="post-title">
+                    <h2 class="medium" v-if="randomPost">{{ randomPost.title }}</h2>
+                </div>
+                <div class="post-text" v-if="randomPost">
+                    {{ randomPost.body }}
+                </div>
+                <div class="post-footer">
+                    <div class="post-date">
+                        {{ postDate }}
+                    </div>
+                    <div class="post-author">
+                        <a href="">{{ author }}</a>
+                    </div>
+                    <div class="post-time-read">
+                        <img src="/storage/img/Clock.svg" class="clock" alt="clock">{{ readTime }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script src="./RandomPost.js"></script>
