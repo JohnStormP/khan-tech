d781eefabec29216ebbe651f2fc0644c827bb527
Http <httpskylogic@gmail.com>
2023-05-23 01:38:21 +0300

    feat: frontend single post block

---
+++ resources/js/Pages/Home/SinglePost/SinglePost.js
@@ -0,0 +1,14 @@
+export default {
+    props: {
+        title: String,
+        image: String,
+        category: String
+    },
+    data() {
+        return {
+            postDate: '28 Feb 2021',
+            author: 'Jim Sullivan',
+            readTime: '6 min read'
+        }
+    }
+}
---
+++ resources/js/Pages/Home/SinglePost/SinglePost.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="col-4 post">
+        <div class="image">
+            <img :src="image" alt="" class="img-fluid"/>
+        </div>
+        <div class="category">
+            {{ category }}
+        </div>
+        <div class="title">
+            {{ title }}
+        </div>
+        <div class="single">
+            <div class="date">
+                {{ postDate }}
+            </div>
+            <div class="author">
+                <a href=""> {{ author }}</a>
+            </div>
+            <div class="time-read">
+                <img src="/storage/img/Clock.svg" class="clock" alt="clock">{{ readTime }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./SinglePost.js"></script>
