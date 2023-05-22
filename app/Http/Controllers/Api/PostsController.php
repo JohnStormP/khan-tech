adf7d7cd4273ce9e20acd982cd5d8f3acd0b058e
Http <httpskylogic@gmail.com>
2023-05-22 10:10:15 +0300

    feat: posts api

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+
+class PostsController extends Controller
+{
+    public function topPosts()
+    {
+        $threePosts = Post::latest()->limit(3)->get();
+        $onePost = Post::latest()->offset(3)->limit(1)->get();
+        $randPost = Post::offset(4)->inRandomOrder()->first();
+        return new PostResource([
+            'latest' => $threePosts,
+            'main' => $onePost,
+            'random' => $randPost
+        ]);
+    }
+
+    public function otherPosts()
+    {
+        return new PostResource(Post::orderBy('id', 'desc')->paginate(5));
+    }
+}
