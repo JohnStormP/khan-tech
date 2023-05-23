34b66c96008f042cad325ccf190fc9fe1078b36a
Http <httpskylogic@gmail.com>
2023-05-23 18:51:11 +0300

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -7,44 +7,18 @@ use Inertia\Inertia;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CategoriesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware(['auth', 'user-role:admin'])->group(function () {
     Route::resource('/posts', PostsController::class);
     Route::post('/posts/{post}/restore', [PostsController::class, 'restore'])->name('posts.restore');
-});
-
-Route::middleware(['auth', 'user-role:admin'])->group(function () {
     Route::resource('/categories', CategoriesController::class)->middleware('auth');
     Route::post('/categories/{category}/restore', [CategoriesController::class, 'restore'])->name('categories.restore');
 });
+
 
 require __DIR__ . '/auth.php';
