e2968703e5fedfa202b6011194db83b22f2f0164
Http <httpskylogic@gmail.com>
2023-06-01 17:34:37 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/js/app.js',
-                'resources/scss/app.js',
+                'resources/css/app.css',
             ],
             refresh: true,
         }),
